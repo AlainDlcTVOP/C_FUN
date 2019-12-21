89f6c8b8634f660405007d09fa00d18901457efb
AlainDLC <jd_alain@hotmail.com>
2019-12-21 20:21:02 +0100

    Simple Comparison

---
+++ C_Fun/Exercise8.1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+
+    a = 6;
+    b = a - 2;
+
+    if (a > b)
+    {
+      printf("%d is grater than %d\n", a, b);
+    }
+    
+    return 0;
+}
