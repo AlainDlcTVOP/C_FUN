e06172c2c9e8cb678fdcfb43e73d8677aece457c
AlainDLC <jd_alain@hotmail.com>
2019-12-21 10:51:54 +0100

    Solution

---
+++ C_Fun/Exercise7.7/main.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+   char a, b, c , d;
+
+   
+
+    a = 'W';
+    b = 'o';
+    c = 'w';
+    d = '\n';
+    printf("%c%c%c%c",a,b,c,d);
+    return 0;
+}
