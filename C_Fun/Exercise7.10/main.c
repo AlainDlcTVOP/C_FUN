ac89a58ce4b49b6a905b70a43d76e8f34b2096c1
AlainDLC <jd_alain@hotmail.com>
2019-12-21 19:23:54 +0100

    Destroy

---
+++ C_Fun/Exercise7.10/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char promt[] = "Program to Destroy the World\nPress Enter to expolde: ";
+   
+    printf("%s ", promt);
+    getchar();
+
+    return 0;
+}
