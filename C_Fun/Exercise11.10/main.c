3b17d1362a45c78652e4762b1921869a0645c5a8
AlainDLC <jd_alain@hotmail.com>
2019-12-22 23:24:48 +0100

    Math

---
+++ C_Fun/Exercise11.10/main.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num;
+
+    for(num = 5; num <100; num+=5)
+    {
+        printf("%d\n", num);
+    }
+
+    return 0;
+}
---
+++ C_Fun/Exercise11.9/main.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+   float alpha;
+ alpha=501;
+ printf("alpha = %.1f\n",alpha);
+ alpha=alpha+99;
+ printf("alpha = %.1f\n",alpha);
+ alpha=alpha-250;
+ printf("alpha = %.1f\n",alpha);
+ alpha=alpha/82;
+ printf("alpha = %.1f\n",alpha);
+ alpha=alpha*4.3;
+ printf("alpha = %.1f\n",alpha);
+
+    return 0;
+}
+
