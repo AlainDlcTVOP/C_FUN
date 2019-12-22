6c4fe33ab3caefb205e1f738513f23624bf17325
AlainDLC <jd_alain@hotmail.com>
2019-12-22 13:06:57 +0100

    magic loop

--- C_Fun/Exercise9.12/main.c
+++ C_Fun/Exercise9.12/main.c
@@ -2,17 +2,28 @@
 
 int main(int argc, char const *argv[])
 {
-    int a, b, c, d;
+    int a, b, c, d, code;
+    char e;
 
     for (a = 'A'; a <= 'Z'; a++)
     {
+        
         for (b = 'A'; b <= 'Z'; b++)
+
         {
             for (c = 'A'; c <= 'Z'; c++)
             {
                 for (d = 'A'; d <= 'Z'; d++)
                 {
-                    printf("%c%c%c\t", a, b, c, d);
+                    for (code = 1; code <= 32; code++)
+                    {
+                        for (e  = '!'; e <= '?'; e++)
+                        {
+                             printf("%c%c%c%%cd%d\t", a, b, c, d,e, code);
+                        }
+                        
+                       
+                    }
                 }
             }
         }
