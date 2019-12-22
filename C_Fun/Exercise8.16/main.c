b7be7a39ef962b89009e3134ca4acaecc441d1c1
AlainDLC <jd_alain@hotmail.com>
2019-12-22 10:08:41 +0100

    Continue

---
+++ C_Fun/Exercise8.16/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+
+int main()
+{
+    char yorn;
+
+    printf("Do you want to continue (Y/N)? ");
+    scanf("%c",&yorn);
+    if( yorn == 'Y' || yorn == 'y' )
+    {
+        puts("Continuing...");
+    }
+    else if( yorn == 'N' || yorn == 'n' )
+    {
+        puts("Stopping now.");
+    }
+    else
+    {
+        puts("You didn't type Y or N!");
+    }
+    return(0);
+}
